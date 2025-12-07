a30521106018f4a04fa20c86d3db301430835ea1
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-07 21:50:30 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -45,7 +45,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "gw" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public[0].id
 }
 
 resource "aws_route_table" "public" {
