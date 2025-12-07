c5bf0eaf96ee432dd3b141c82e03c7dca80f4fe4
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-07 21:46:13 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -172,7 +172,7 @@ resource "aws_security_group" "instance_sg" {
 resource "aws_instance" "graviton_box" {
   ami                         = data.aws_ami.al2023_arm64.id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public.id
+  subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.instance_sg.id]
   associate_public_ip_address = true
   key_name                    = var.key_name
