0fd9cf132f1bdfb385a692a3b2518b08710ef991
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-10 00:14:37 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -170,7 +170,7 @@ resource "aws_instance" "graviton_box" {
   user_data = <<_DATA
 #!/bin/bash
 echo "install code-server ..."
-curl -fsSL https://code-server.dev/install.sh | sh
+sudo su - ec2-user -c "curl -fsSL https://code-server.dev/install.sh | sh"
 
 _DATA
 
