58ea2d9e839ea865321f8ff6bda08a6ce9ed17b2
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-09 23:54:32 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -170,16 +170,7 @@ resource "aws_instance" "graviton_box" {
   user_data = <<_DATA
 #!/bin/bash
 
-# Install VS Code Server (Code Server) - 설치하면 자동으로 systemctl 에 등록된다.
-echo "install code-server ..."
-curl -fsSL https://code-server.dev/install.sh | sh
-sudo systemctl enable --now code-server@ec2-user
 
-CONFIG_FILE="/home/ec2-user/.config/code-server/config.yaml"
-sed -i 's/^\s*bind-addr:\s*127.0.0.1:8080/bind-addr: 0.0.0.0:8080/g' "$CONFIG_FILE"
-sed -i 's/^\s*auth:\s*password/auth: none/g' "$CONFIG_FILE"
-
-sudo systemctl restart code-server@ec2-user  
 _DATA
 
   tags = {
