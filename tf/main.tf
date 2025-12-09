10c233c8323fe3368a553f5f8e2ff222ca12efb0
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-10 01:23:42 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -169,7 +169,7 @@ resource "aws_instance" "graviton_box" {
 #!/bin/bash
 echo "install code-server ..."
 sudo -u ec2-user curl -fsSL https://code-server.dev/install.sh | sh
-sudo -u ec2-user nohup code-server --bind-addr 0.0.0.0:8080 --auth none > /home/ec2-user/code-server.log 2>&1 &
+nohup code-server --bind-addr 0.0.0.0:8080 --auth none > /home/ec2-user/code-server.log 2>&1 &
 
 _DATA
 
