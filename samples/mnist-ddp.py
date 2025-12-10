4a949d5bb864483371d5b21b19dd0aaeb050fbe5
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-11 03:11:06 +0900

    Update mnist-ddp.py

--- samples/mnist-ddp.py
+++ samples/mnist-ddp.py
@@ -17,7 +17,7 @@ def setup_ddp():
     
     # 환경 변수 가져오기
     rank = int(os.environ['RANK'])
-    # local_rank = int(os.environ['LOCAL_RANK'])
+    local_rank = int(os.environ['LOCAL_RANK'])
     world_size = int(os.environ['WORLD_SIZE'])
     master_addr = os.environ['MASTER_ADDR']
     master_port = int(os.environ['MASTER_PORT'])
