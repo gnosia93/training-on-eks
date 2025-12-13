fa19fa80ad8c7b3cb861edafe7362d621ea50a88
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-13 14:11:14 +0900

    Update outputs.tf

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -2,18 +2,16 @@ output "graviton_dns" {
   value       = aws_instance.graviton_box.public_dns
   description = "SSH 및 VS Code Server 접속을 위한 EC2 인스턴스의 공인 IP 주소"
 }
+output "graviton_vscode" {
+    value = "http://${aws_instance.graviton_box.public_dns}:8080"
+    description = "브라우저에서 VS Code 서버에 접속할 수 있는 URL (PW: 'password' by default)"
+}
 
 output "x86_dns" {
   value       = aws_instance.x86_box.public_dns
   description = "SSH 및 VS Code Server 접속을 위한 EC2 인스턴스의 공인 IP 주소"
 }
-
-output "vscode_graviton" {
-    value = "http://${aws_instance.graviton_box.public_dns}:8080"
-    description = "브라우저에서 VS Code 서버에 접속할 수 있는 URL (PW: 'password' by default)"
-}
-
-output "vscode_x86" {
+output "x86_vscode" {
     value = "http://${aws_instance.x86_box.public_dns}:8080"
     description = "브라우저에서 VS Code 서버에 접속할 수 있는 URL (PW: 'password' by default)"
 }
