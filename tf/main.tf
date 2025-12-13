7eabd3ae990d8a76d93879b444dfa50f65b3ce2c
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-13 21:33:40 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -190,7 +190,6 @@ sudo systemctl enable --now code-server@ec2-user
 sed -i 's/127.0.0.1/0.0.0.0/g; s/auth: password/auth: none/g' /home/ec2-user/.config/code-server/config.yaml
 EC2_USER_SCRIPT
 
-sed -i 's/127.0.0.1/0.0.0.0/g; s/auth: password/auth: none/g' /home/ec2-user/.config/code-server/config.yaml
 sudo systemctl start code-server@ec2-user
 _DATA
 
