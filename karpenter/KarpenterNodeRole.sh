9f214761e8e31ad124d1cdf507b841cb8a8a9aa9
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-14 08:54:23 +0900

    Update KarpenterNodeRole.sh

--- karpenter/KarpenterNodeRole.sh
+++ karpenter/KarpenterNodeRole.sh
@@ -13,3 +13,16 @@ echo '{
 
 aws iam create-role --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
     --assume-role-policy-document file://node-trust-policy.json
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonEKS_CNI_Policy"
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn "arn:${AWS_PARTITION}:iam::aws:policy/AmazonSSMManagedInstanceCore"
+
