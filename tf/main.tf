f78dc8427c8fd27ba73b7dc9f27f76fa6beaa8fa
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-13 21:21:36 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -185,7 +185,8 @@ resource "aws_instance" "graviton_box" {
   user_data = <<_DATA
 #!/bin/bash
 sudo -u ec2-user -i <<'EC2_USER_SCRIPT'
-curl -fsSL https://code-server.dev/install.sh | sh && sudo systemctl enable --now code-server@ec2-user
+curl -fsSL https://code-server.dev/install.sh | sh && \
+sudo systemctl enable --now code-server@ec2-user && \
 sed -i 's/127.0.0.1/0.0.0.0/g; s/auth: password/auth: none/g' /home/ec2-user/.config/code-server/config.yaml
 EC2_USER_SCRIPT
 
