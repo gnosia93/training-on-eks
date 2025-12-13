d92ede843b9b4f9e17ca77c217b0dd077c4afd5a
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-13 21:07:18 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -216,10 +216,10 @@ resource "aws_instance" "x86_box" {
 #!/bin/bash
 sudo -u ec2-user -i <<'EC2_USER_SCRIPT'
 curl -fsSL https://code-server.dev/install.sh | sh && sudo systemctl enable --now code-server@ec2-user
-EC2_USER_SCRIPT
-
 sed -i 's/127.0.0.1/0.0.0.0/g' /home/ec2-user/.config/code-server/config.yaml
 sed -i 's/auth: password/auth: none/g' /home/ec2-user/.config/code-server/config.yaml
+EC2_USER_SCRIPT
+
 sudo systemctl start code-server@ec2-user
 
 _DATA
