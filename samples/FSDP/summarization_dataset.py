7cdd577bb33fa359962641096d59faceb52effeb
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-16 18:09:37 +0900

    Update summarization_dataset.py

--- samples/FSDP/summarization_dataset.py
+++ samples/FSDP/summarization_dataset.py
@@ -14,7 +14,7 @@ from string import punctuation
 
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+#from torch.utils.data import Dataset, DataLoader
 
 from transformers import (
     T5ForConditionalGeneration,
