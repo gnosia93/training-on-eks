502abfa9d9a39f7849ca7260180ddbf6229b1d94
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-17 00:31:49 +0900

    Delete samples/FSDP directory

--- samples/FSDP/download_dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 1. 디렉토리 생성 (없을 경우)
-mkdir -p data/wikihow
-
-# 2. 데이터 다운로드
-wget -P data/wikihow https://public-nlp-datasets.s3.us-west-2.amazonaws.com/wikihowAll.csv
-wget -P data/wikihow https://public-nlp-datasets.s3.us-west-2.amazonaws.com/wikihowSep.csv
