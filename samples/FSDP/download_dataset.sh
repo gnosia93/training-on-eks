4b58e229bc4231891db187db6d98568a8aa91da3
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-16 16:31:46 +0900

    Update download_dataset.sh

--- samples/FSDP/download_dataset.sh
+++ samples/FSDP/download_dataset.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Create the "data" folder if it doesn't exist
+# 1. 디렉토리 생성 (없을 경우)
 mkdir -p data/wikihow
 
-# Download the files into the "data" folder
+# 2. 데이터 다운로드
 wget -P data/wikihow https://public-nlp-datasets.s3.us-west-2.amazonaws.com/wikihowAll.csv
 wget -P data/wikihow https://public-nlp-datasets.s3.us-west-2.amazonaws.com/wikihowSep.csv
