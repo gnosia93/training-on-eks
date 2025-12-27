9ca74de6f72959c543c451e09ccfac066cad496c
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-12-28 01:56:50 +0900

    Update llama-3-8b.py

--- samples/deepspeed/llama-3-8b.py
+++ samples/deepspeed/llama-3-8b.py
@@ -9,7 +9,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
 # 2. 아주 큰 모델을 초기화할 때 메모리 효율을 위해 'meta' 장치 사용
-# ZeRO-3는 이 설정을 통해 모델을 로드하면서 즉시 GPU들에 분산시킵니다.
+# ZeRO-3는 이 설정을 통해 모델을 로드하면서 즉시 GPU들에 분산시킨다.
 config = AutoConfig.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_config(config) 
 
@@ -25,14 +25,14 @@ tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns
 training_args = TrainingArguments(
     output_dir="./llama3-deepspeed",
     per_device_train_batch_size=4,
-    gradient_accumulation_steps=4, # 실제 배치 사이즈 = 4 * 4 * GPU개수
+    gradient_accumulation_steps=4,                 # 실제 배치 사이즈 = 4 * 4 * GPU개수
     learning_rate=2e-5,
     num_train_epochs=3,
-    bf16=True,                    # A100/H100/B200 GPU 권장
+    bf16=True,                                     # A100/H100/B200 GPU 권장
     logging_steps=10,
-    deepspeed="ds_config_stage3.json", # 위에서 만든 설정 파일 연결
+    deepspeed="llama-3-8b-stage3.json", 
     save_strategy="epoch",
-    gradient_checkpointing=True,   # 메모리 절약을 위한 재계산(FlashAttention과 유사한 개념)
+    gradient_checkpointing=True,                   # 메모리 절약을 위한 재계산
 )
 
 # 5. 트레이너 실행
