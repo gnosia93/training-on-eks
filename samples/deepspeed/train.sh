4769491d17b0cdbd82c13823703d50a8b3a8f8a0
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-01 20:25:15 +0900

    Update train.sh

--- samples/deepspeed/train.sh
+++ samples/deepspeed/train.sh
@@ -1,3 +1,11 @@
+# 1. HF_TOKEN 환경 변수 체크
+if [ -z "${HF_TOKEN}" ]; then
+    echo "Error: HF_TOKEN 환경 변수가 설정되지 않았습니다."
+    echo "Llama-3 모델 학습을 위해 Hugging Face 토큰이 필요합니다."
+    echo "export HF_TOKEN='your_token_here' 명령으로 토큰을 설정해주세요."
+    exit 1  # 스크립트 실행 중단 (종료)
+fi
+
 export AWS_REGION=$(aws ec2 describe-availability-zones --query "AvailabilityZones[0].RegionName" --output text)
 export INSTANCE_TYPE=g6e.12xlarge              # 훈련 인스턴스 타입   (g6e.48xlarge)
 export AZ=${AWS_REGION}a                 
@@ -8,7 +16,7 @@ export EFA_PER_NODE=1                          # 100Gbp 사용
 
 cd ~/training-on-eks/samples/deepspeed
 kubectl get trainjob 
-kubectl delete trainjob llama-3-8b
+kubectl delete trainjob llama-3-8b --ignore-not-found=true # 작업이 없어도 에러 없이 진행
 envsubst '$INSTANCE_TYPE $NODE_NUM $GPU_PER_NODE $EFA_PER_NODE $HF_TOKEN' < trainjob-1to1.yaml | kubectl apply -f - 
 
 kubectl get trainjob 
