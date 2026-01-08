07fcd25727ca33bfdc82806118ad6e05c8687d3f
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-08 19:34:39 +0900

    Update cpu-llama3.py

--- samples/cpu-amx/cpu-llama3.py
+++ samples/cpu-amx/cpu-llama3.py
@@ -9,6 +9,7 @@ from transformers import (
     DataCollatorForLanguageModeling
 )
 from datasets import load_dataset
+import deepspeed
 
 def main():
 
@@ -20,6 +21,7 @@ def main():
 
     model_name = "meta-llama/Meta-Llama-3-8B"
     output_dir = "./llama3-training-output" 
+    ds_config_path = "cpu-ds.json"
 
     # 토크나이저 로드
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -43,37 +45,35 @@ def main():
 
     # Data Collator 추가: labels를 자동으로 생성해줌 (허깅페이스의 경우 인풋과 레이블은 동일하다. 모델 내부에서 엇깔리게 처리함)
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
-
-    # 4. 학습 인자 설정 (DeepSpeed + IPEX + CPU 분산)
+    
     training_args = TrainingArguments(
-        local_rank=local_rank,                         # deepspeed 가 train batch size 를 제대로 인식하지 못해서 분산환경임을 알려주기 위해 추가
+        local_rank=local_rank,                   # deepspeed 가 gloo 환경에서 train batch size 를 제대로 인식하지 못해서 분산환경임을 알려주기 위해 추가
 
         output_dir=output_dir,
         overwrite_output_dir=True,
         do_train=True,
         
-        # CPU 및 최적화 설정
+        deepspeed=ds_config_path,
         use_cpu=True,
-        bf16=True,                                     # 인텔 4/5세대 Xeon AMX 가속 활용        
-        deepspeed="cpu-ds.json",
-        
-        # 분산 학습 파라미터
-        per_device_train_batch_size=1,
-        gradient_accumulation_steps=4,
         learning_rate=2e-5,
         num_train_epochs=1,
         save_steps=100,
         max_steps=50,                                  # 딱 50번의 스텝만 하고 종료 / 이경우 에포크는 무시됨   
         logging_steps=1,
+
+        # 딥스피드와 중복된것은 제거 
+        # bf16=True,                                     # 인텔 4/5세대 Xeon AMX 가속 활용        
+        #per_device_train_batch_size=1,
+        #gradient_accumulation_steps=4,
     )
 
-    # 5. 모델 로드 (DeepSpeed가 모델을 쪼개서 로드하도록 처리)
-    # ZeRO-3를 사용할 때 모델을 바로 생성하지 않고 Trainer 내부에서 처리하게 함
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        dtype=torch.bfloat16,
-        device_map=None # 분산 학습 시 필수
-    )
+    # 모델 로드 (DeepSpeed가 모델을 쪼개서 로드하도록 처리)
+    with deepspeed.zero.Init(config_dict_or_path=ds_config_path):
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            dtype=torch.bfloat16,
+            device_map=None                    # 딥스피드가 분산 파라미터 로딩시 매핑하도록 함. 
+        )
 
     # 트레이너 초기화 및 학습 시작
     trainer = Trainer(
@@ -84,7 +84,7 @@ def main():
         data_collator=data_collator,            # 반드시 포함되어야 함
     )
 
-    print("--- 학습 시작 ---")
+    print(f"--- {local_rank} RANK 학습 시작 ---")
     trainer.train()
     
     # trainer.save_model(output_dir)
